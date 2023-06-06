e8edfcecc35fd99382821aa19c8ae2af33c378f1
Prashanth Erumalla <naveennagam6015@gmail.com>
2023-06-06 10:24:13 +0530

    month

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var person = {name:'Naveen',age:21,class:15};
+console.log(person.name);
+document.write(person.age)
+
+//   for (let prop in document.body.childNodes) alert(prop);
+
+let month = prompt("Select a month");
+if (month > 12) {
+    console.log("Please select a valid  month");
+} else if (month >= 10) {
+    console.log("Winter");
+} else if (month >= 6) {
+    console.log("Rainy");
+} else if (month >= 3) {
+    console.log("Summer")
+} else if (month < 3) {
+    console.log("Winter")
+} else {
+    console.log("Select A month")
+}
